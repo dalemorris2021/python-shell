fef1d6578e5ba6eeeafec252e6a6b0543d47ef63
Dale Morris <dalemorris2021@gmail.com>
2025-02-16 14:24:49 -0500

    Now convert contents to list of ints and back

--- shell.py
+++ shell.py
@@ -3,18 +3,19 @@ import sys
 
 PROGRAM_NAME = sys.argv[0]
 MAX_FILE_DATA = 4096
-HEADER = 'XX:                1               2               3\nXX:0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF\n'
+HEADER = '''XX:                1               2               3
+XX:0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF\n'''
 
 class Config:
     def __init__(self, in_file):
         self.in_file = in_file
 
 
-def leftPad(n: int, char: str, s: str) -> str:
+def leftPad(s: str, char: str, n: int) -> str:
     return char * (n - len(s)) + s
 
 
-def contentsToRaw(contents: str) -> list[list[str]]:
+def contentsToRaw(contents: str) -> list[list[int]]:
     temp1 = contents.splitlines()[2:]
     for i in range(len(temp1)):
         temp1[i] = '0' + temp1[i][3:len(temp1[i])-1]
@@ -23,18 +24,27 @@ def contentsToRaw(contents: str) -> list[list[str]]:
     for i in range(len(temp1)):
         raw.append([temp1[i][j:j+2] for j in range(0, len(temp1[i]), 2)])
     
+    for i in range(len(raw)):
+        for j in range(len(raw[0])):
+            raw[i][j] = int(raw[i][j], 16)
+    
     return raw
 
 
-def rawToContents(raw: list[list[str]]) -> str:
+def rawToContents(raw: list[list[int]]) -> str:
     temp1 = []
     for i in range(len(raw)):
-        temp1.append(''.join(raw[i]))
-    for i in range(len(temp1)):
-        temp1[i] = leftPad(2, '0', hex(i)[2:].upper()) + ':' + temp1[i][1:] + '0'
+        temp1.append([])
+        for j in range(len(raw[0])):
+            temp1[i].append(leftPad(hex(raw[i][j])[2:], '0', 2).upper())
     
-    contents = HEADER
-    contents += '\n'.join(temp1)
+    temp2 = []
+    for i in range(len(temp1)):
+        temp2.append(''.join(temp1[i]))
+    for i in range(len(temp2)):
+        temp2[i] = leftPad(hex(i)[2:].upper(), '0', 2) + ':' + temp2[i][1:] + '0'
+    
+    contents = HEADER + '\n'.join(temp2)
     
     return contents
 
